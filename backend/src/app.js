e67cef8d37d5231a6ab5949b042327a1ba71bd0b
Jai-Pradeep <jjaipradeep@gmail.com>
2026-06-09 23:29:05 +0530

    Render deployed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,41 +1,41 @@
+const express = require("express")
+const cors = require("cors")
 require("dotenv").config();
 
-// const express = require("express")
-// const cors = require("cors")
-// require("dotenv").config();
+const app = express();
 
-// const app = express();
+app.use(cors({
+  exposedHeaders: ["Content-Disposition"]
+}));
+app.use(express.json());
 
-// app.use(cors({
-//   exposedHeaders: ["Content-Disposition"]
-// }));
-// app.use(express.json());
+const authRoutes = require("./routes/authRoutes");
+const worklogRoutes = require("./routes/worklogRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
-// const authRoutes = require("./routes/authRoutes");
-// const worklogRoutes = require("./routes/worklogRoutes");
-// const adminRoutes = require("./routes/adminRoutes");
+app.use("/auth", authRoutes);
+app.use("/worklogs", worklogRoutes);
+app.use("/admin", adminRoutes);
 
-// app.use("/auth", authRoutes);
-// app.use("/worklogs", worklogRoutes);
-// app.use("/admin", adminRoutes);
+const PORT = process.env.PORT || 5000;
 
-// const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+
 //--------------------------TEST DB CONNECTION------------------------------------
 // Test DB Connection
-const pool = require("./config/db"); // adjust path if needed
+// const pool = require("./config/db"); // adjust path if needed
 
-pool.query("SELECT NOW()")
-  .then(res => {
-    console.log("✅ DB Connected");
-    console.log(res.rows);
-  })
-  .catch(err => {
-    console.error("❌ DB Error");
-    console.error(err);
-  });
+// pool.query("SELECT NOW()")
+//   .then(res => {
+//     console.log("✅ DB Connected");
+//     console.log(res.rows);
+//   })
+//   .catch(err => {
+//     console.error("❌ DB Error");
+//     console.error(err);
+//   });
 
   
